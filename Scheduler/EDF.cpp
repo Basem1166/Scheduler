73b7eae7497ba54a75d46b3133b2940c8d0585ef
Alii-s <119129254+Alii-s@users.noreply.github.com>
2023-05-12 11:54:06 +0300

    Count of processes that terminated before deadline
    ADDED IT IN SCHEDULER

--- Scheduler/EDF.cpp
+++ Scheduler/EDF.cpp
@@ -24,10 +24,6 @@ void EDF::ScheduleAlgo()
 		RunningProcess->setTerminationT(pScheduler->getTime());
 		RunningProcess->setTRT();
 
-		if (pScheduler->getTime() < RunningProcess->getDeadline())
-		{
-			BeforeDeadline++;
-		}
 
 		pScheduler->AddtoTRM(RunningProcess);
 		RunningProcess = nullptr;
